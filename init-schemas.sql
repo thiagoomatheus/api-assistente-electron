44d6f8d48aa16b88eaac98960dd99b360fdd85e4
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-06-16 13:11:36 -0300

    adicionar condiguração de prisma

---
+++ init-schemas.sql
@@ -0,0 +1,10 @@
+-- Criar os esquemas necessários
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_namespace WHERE nspname = 'api_scheme') THEN
+    CREATE SCHEMA api_scheme;
+  END IF;
+  IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_namespace WHERE nspname = 'evolution_api') THEN
+    CREATE SCHEMA evolution_api;
+  END IF;
+END $$;
