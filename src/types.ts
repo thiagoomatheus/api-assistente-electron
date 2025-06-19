a7cf6b9fe48c6f8792ead3ab92c4254dfc95a102
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-06-18 23:59:06 -0300

    adicionar tipo AsaasClient

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,30 @@ export type FastifyTypedInstance = FastifyInstance<
     FastifyBaseLogger,
     ZodTypeProvider
 >
+
+export type AsaasClient = {
+  object: string,
+  id: string,
+  dateCreated: string,
+  name: string,
+  email: string,
+  phone: string,
+  mobilePhone: string,
+  address: string,
+  addressNumber: string,
+  complement: string,
+  province: string,
+  city: string,
+  cityName: string,
+  state: string,
+  country: string,
+  postalCode: string,
+  cpfCnpj: string,
+  personType: string,
+  deleted: boolean,
+  additionalEmails: string,
+  externalReference: string,
+  notificationDisabled: boolean,
+  observations: string,
+  foreignCustomer: boolean
+}
