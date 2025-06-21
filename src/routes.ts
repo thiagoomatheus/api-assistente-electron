d611e26c586b54fe292c164398231fcda47911c3
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-06-20 21:37:54 -0300

    corrigir erro env contendo $

--- src/routes.ts
+++ src/routes.ts
@@ -61,7 +61,7 @@ export default async function routes(app: FastifyTypedInstance) {
                 headers: {
                     'Content-Type': 'application/json',
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                    'access_token': `${process.env.ASAAS_API_KEY}`
+                    'access_token': `$${process.env.ASAAS_API_KEY}`
                 }
             })
 
@@ -153,7 +153,7 @@ export default async function routes(app: FastifyTypedInstance) {
                     headers: {
                         'Content-Type': 'application/json',
                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                        'access_token': `${process.env.ASAAS_API_KEY}`
+                        'access_token': `$${process.env.ASAAS_API_KEY}`
                     }
                 })
 
